d1e98bfd17e9c0940d0640e8df0f6916277b1f10
Semo Mironov <mironov5712s@gmail.com>
2023-09-01 16:27:59 +0300

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,7 +1,9 @@
-let country = "Bulgaria";
-let continent = "Europe";
-let population = 6800000;
-
-console.log(country);
-console.log(continent);
-console.log(population);
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+const BMIMark = massMark / (heightMark * heightMark);
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+const markHigherBMI = BMIMark > BMIJohn;
+console.log(markHigherBMI);

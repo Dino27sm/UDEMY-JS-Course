41a7b9ecfa6277fae34bd98f355e3eb96cd685e9
Semo Mironov <mironov5712s@gmail.com>
2023-09-02 12:48:56 +0300

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -6,7 +6,7 @@ const BMIMark = massMark / (heightMark * heightMark);
 const BMIJohn = massJohn / (heightJohn * heightJohn);
 console.log(BMIMark, BMIJohn);
 if (BMIMark > BMIJohn) {
-  console.log("Mark's BMI is higher than John's!");
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
 } else {
-  console.log("John's BMI is higher than Mark's!");
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
 }

fdc43203d191c389d8270d7547b1b9992b5a26d5
Semo Mironov <mironov5712s@gmail.com>
2023-09-12 12:44:58 +0300

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -19,27 +19,46 @@
 // console.log(favourite);
 // console.log(typeof favourite);
 //
+// let day = prompt("Enter the current day, please:");
+// console.log(day);
+// //
+// switch (day) {
+//   case "monday":
+//     console.log("Today is monday.");
+//     break;
+//   case "tuesday":
+//     console.log("Today is tuesday.");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Today is wednesday or thursday.");
+//     break;
+//   case "friday":
+//     console.log("Today is friday.");
+//     breack;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Today is saturday or sunday.");
+//     break;
+//   default:
+//     console.log("Not a valid day");
+// }
+// *****************************************************
+// The same Exercise with IF statement
 let day = prompt("Enter the current day, please:");
 console.log(day);
 //
-switch (day) {
-  case "monday":
-    console.log("Today is monday.");
-    break;
-  case "tuesday":
-    console.log("Today is tuesday.");
-    break;
-  case "wednesday":
-  case "thursday":
-    console.log("Today is wednesday or thursday.");
-    break;
-  case "friday":
-    console.log("Today is friday.");
-    breack;
-  case "saturday":
-  case "sunday":
-    console.log("Today is saturday or sunday.");
-    break;
-  default:
-    console.log("Not a valid day");
+if (day === "monday") {
+  console.log("Today is monday.");
+} else if (day === "tuesday") {
+  console.log("Today is tuesday.");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Today is wednesday or thursday.");
+} else if (day === "friday") {
+  console.log("Today is friday.");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("Today is saturday or sunday.");
+} else {
+  console.log("Not a valid day");
 }
+//

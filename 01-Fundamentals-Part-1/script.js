d57f5fc607fb53a542e38ea002e4665fb4921df3
Semo Mironov <mironov5712s@gmail.com>
2023-09-13 11:40:06 +0300

    Update script.js

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -70,9 +70,11 @@
 // console.log(`I may drink ${age >= 20 ? "beer" : "water"}`);
 //****************************************************** */
 // Ternary Exercise
-const bill = 275;
+// const bill = 275;
 
-let tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
-console.log(
-  `the bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`
-);
+// let tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+// console.log(
+//   `the bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}.`
+// );
+//******************************************************* */
+//

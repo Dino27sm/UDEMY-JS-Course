8f7625b9c3df1961d53551472190006e841cf94f
Semo Mironov <mironov5712s@gmail.com>
2023-09-19 12:42:50 +0300

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -20,10 +20,16 @@ function calcAge1(birthYear) {
 const age1 = calcAge1(1991);
 //
 // *********************************************
+//
 //  FUNCTION EXPRESSION
 const calcAge2 = function (birthYear) {
   return 2037 - birthYear;
 };
 const age2 = calcAge2(1991);
+//******************************************** */
 //
-console.log(age1, age2);
+//  ARROW FUNCTION
+const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+//********************************************* */
+console.log(age1, age2, age3);

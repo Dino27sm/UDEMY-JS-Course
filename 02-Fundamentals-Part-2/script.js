ee8f3fe4056bb6ae2dffedcb9f1f150987f68cf3
Semo Mironov <mironov5712s@gmail.com>
2023-09-25 17:12:33 +0300

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -14,22 +14,40 @@
 //******************************************* */
 //
 //  FUNCTION DECLARATION
-function calcAge1(birthYear) {
-  return 2037 - birthYear;
-}
-const age1 = calcAge1(1991);
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+// const age1 = calcAge1(1991);
 //
 // *********************************************
 //
 //  FUNCTION EXPRESSION
-const calcAge2 = function (birthYear) {
-  return 2037 - birthYear;
-};
-const age2 = calcAge2(1991);
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+// const age2 = calcAge2(1991);
 //******************************************** */
 //
 //  ARROW FUNCTION
-const calcAge3 = (birthYear) => 2037 - birthYear;
-const age3 = calcAge3(1991);
+// const calcAge3 = (birthYear) => 2037 - birthYear;
+// const age3 = calcAge3(1991);
 //********************************************* */
-console.log(age1, age2, age3);
+// console.log(age1, age2, age3);
+//
+//******************************************** */
+//=============  CHALLENGE  =====================
+function checkWinner(avgDolphins, avgKoalas) {
+  if (avgDolphins >= 2 * avgKoalas) {
+    return `Dolphins win (${avgDolphins} vs ${avgKoalas}).`;
+  } else if (avgKoalas >= 2 * avgDolphins) {
+    return `Koalas win (${avgKoalas} vs ${avgDolphins}).`;
+  } else {
+    return `No team wins.`;
+  }
+}
+//
+const calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+const scoreDolphins = calcAverage(85, 54, 41);
+const scoreKoalas = calcAverage(23, 34, 27);
+//
+console.log(checkWinner(scoreDolphins, scoreKoalas));

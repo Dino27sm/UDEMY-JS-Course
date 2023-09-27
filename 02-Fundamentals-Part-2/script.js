55338814467f7cc66ed74cd4358542c611bbace8
Semo Mironov <mironov5712s@gmail.com>
2023-09-27 22:54:50 +0300

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -96,12 +96,22 @@
 // };
 //=========  With ARROW Function =========================
 //
-const calcTip = (bill) =>
-  bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// const calcTip = (bill) =>
+//   bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill;
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// //
+// console.log(bills);
+// console.log(tips);
+// console.log(totals);
 //
-console.log(bills);
-console.log(tips);
-console.log(totals);
+//========== OBJECT in JS =================================
+//
+const myObject = {
+  firstName: "Ivan",
+  lastName: "Nikolov",
+  age: 2023 - 1983,
+  job: "teacher",
+  friends: ["Ann", "Eva", "Mary"],
+};

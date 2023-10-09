ece499644ff667d5fe36cbd1030acbb9a45ac8db
Semo Mironov <mironov5712s@gmail.com>
2023-10-09 13:36:19 +0300

    Update script.js

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -183,30 +183,35 @@
 //
 //====================  CHALLANGE #3  =================
 //
-const mark = {
-  fullName: "Mark Miller",
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  },
-};
-const john = {
-  fullName: "John Smith",
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    this.bmi = this.mass / (this.height * this.height);
-    return this.bmi;
-  },
-};
+// const mark = {
+//   fullName: "Mark Miller",
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   },
+// };
+// const john = {
+//   fullName: "John Smith",
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     this.bmi = this.mass / (this.height * this.height);
+//     return this.bmi;
+//   },
+// };
+// //
+// console.log(
+//   mark.calcBMI() >= john.calcBMI()
+//     ? `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
+//     : `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
+// );
+// //
+// console.log(mark);
+// console.log(john);
 //
-console.log(
-  mark.calcBMI() >= john.calcBMI()
-    ? `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
-    : `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
-);
-//
-console.log(mark);
-console.log(john);
+//==================  FOR Loop  ==========================
+for (let rep = 1; rep <= 5; rep++) {
+  console.log(`This is line number: ${rep}`);
+}

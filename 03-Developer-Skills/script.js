3b2faf82acb4b20c677312a0e77efa833c395151
Semo Mironov <mironov5712s@gmail.com>
2023-10-25 12:15:30 +0300

    Update script.js

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,6 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-const x = 13;
-const y = 14;
-console.log(x, y);
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  let out_array = [];
+  for (let i = 0; i < l.length; i++) {
+    if (typeof l[i] === "number") {
+      out_array.push(l[i]);
+    }
+  }
+  return out_array;
+}
+console.log(filter_list([1, 2, 17, "text1", "23", 37]));

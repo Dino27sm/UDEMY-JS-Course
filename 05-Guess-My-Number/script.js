d3b44774f93e5087f707e5b655402c3165a663ca
Semo Mironov <mironov5712s@gmail.com>
2023-11-10 05:28:51 +0200

    Update script.js

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,10 +1,19 @@
 'use strict';
 //
+//================= Guess my Number =============================
+const secretNumber = Math.trunc(Math.random() * 20 + 1);
+document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '👍 Correct Number!';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = '📉 Too low!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = '📈 Too high!';
   }
 });

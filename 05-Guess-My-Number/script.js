eefa4ea980eb39ef2524ac31cc48665d6ebab8e9
Semo Mironov <mironov5712s@gmail.com>
2023-11-11 06:10:28 +0200

    Update script.js

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -8,15 +8,20 @@ document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //------------ When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
   } else {
+    //---------- When player wins
     if (guess === secretNumber) {
       document.querySelector('.message').textContent = '👍 Correct Number!';
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '30rem';
     } else {
       if (score > 1) {
         score--;
         document.querySelector('.score').textContent = score;
+        //----------- When guess is too HIGH or LOW
         document.querySelector('.message').textContent =
           guess < secretNumber ? '📉 Too low!' : '📈 Too high!';
       } else {

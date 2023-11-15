5798c980ec69b6c3938354314e70b4998bed2003
Semo Mironov <mironov5712s@gmail.com>
2023-11-15 14:32:23 +0200

    Update script.js

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -1,6 +1,9 @@
 'use strict';
 
 //------------- Selecting elements from DOM
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -17,6 +20,7 @@ score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
 let currentScore = 0;
+let activePlayer = 0;
 
 //------------ Rolling DICE functionality
 btnRoll.addEventListener('click', function () {
@@ -31,8 +35,16 @@ btnRoll.addEventListener('click', function () {
   if (dice !== 1) {
     // Add dice value to the
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // Switch to next player
+    currentScore = 0;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });

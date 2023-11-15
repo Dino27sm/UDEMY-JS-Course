ddcae2b08795c7f9fb4f9a29136186ecdf080da9
Semo Mironov <mironov5712s@gmail.com>
2023-11-15 15:09:50 +0200

    Update script.js

--- 07-Pig-Game/script.js
+++ 07-Pig-Game/script.js
@@ -19,8 +19,19 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+
+const switchPlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent =
+    currentScore;
+
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 //------------ Rolling DICE functionality
 btnRoll.addEventListener('click', function () {
@@ -39,12 +50,16 @@ btnRoll.addEventListener('click', function () {
       currentScore;
   } else {
     // Switch to next player
-    currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore;
-
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player0El.classList.toggle('player--active');
-    player1El.classList.toggle('player--active');
+    switchPlayer();
   }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to the active player totall score
+  scores[activePlayer] += currentScore;
+
+  // 3. Switch to next player
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+  switchPlayer();
 });

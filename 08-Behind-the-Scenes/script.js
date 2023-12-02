5408a3c5196828857a9df3e785158ee6d8883731
Semo Mironov <mironov5712s@gmail.com>
2023-12-02 18:31:46 +0200

    Update script.js

--- 08-Behind-the-Scenes/script.js
+++ 08-Behind-the-Scenes/script.js
@@ -1,1 +1,33 @@
 'use strict';
+
+//------ Variable declarations ----------------
+// console.log(firstName);
+// console.log(myAge);
+// console.log(thisYear);
+
+// var firstName = 'Dino';
+// let myAge = 65;
+// const thisYear = 2023;
+
+//------- Functions ---------------------------
+// console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+// function addDecl(a, b) {
+//   return a + b;
+// }
+
+// const addExpr = function (a, b) {
+//   return a + b;
+// };
+
+// var addArrow = (a, b) => a + b;
+
+//-------- Example
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);

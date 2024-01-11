2140796feffd0b091854bec92fa6985b2779f427
Semo Mironov <mironov5712s@gmail.com>
2024-01-11 13:30:51 +0200

    Update script.js

--- 09-Data-Structures-Operators/script.js
+++ 09-Data-Structures-Operators/script.js
@@ -49,27 +49,27 @@ const restaurant = {
 };
 
 //===================================================================
-//--------- Looping OBJECTS
-const properties = Object.keys(openingHours);
-console.log(properties);
+// //--------- Looping OBJECTS
+// const properties = Object.keys(openingHours);
+// console.log(properties);
 
-let openStr = `We are open on ${properties.length} days: `;
-for (const day of properties) {
-  openStr += `${day}, `;
-}
-console.log(openStr);
+// let openStr = `We are open on ${properties.length} days: `;
+// for (const day of properties) {
+//   openStr += `${day}, `;
+// }
+// console.log(openStr);
 
-// console.log(Object.keys(openingHours));
-// console.log(Object.values(openingHours));
+// // console.log(Object.keys(openingHours));
+// // console.log(Object.values(openingHours));
 
-//------- Entire object
-const objEntries = Object.entries(openingHours);
-console.log(objEntries);
+// //------- Entire object
+// const objEntries = Object.entries(openingHours);
+// console.log(objEntries);
 
-for (const [day, { open, close }] of objEntries) {
-  // Her is used destructuring of obect Entries - keys and values
-  console.log(`On ${day} we open at ${open} and close at ${close}.`);
-}
+// for (const [day, { open, close }] of objEntries) {
+//   // Her is used destructuring of obect Entries - keys and values
+//   console.log(`On ${day} we open at ${open} and close at ${close}.`);
+// }
 
 //
 //===================================================================
@@ -250,3 +250,70 @@ for (const [day, { open, close }] of objEntries) {
 //   console.log(`${i + 1}: ${elm}`);
 // }
 //===============================================================
+//
+//----------- Coding CHALLENGE #2 ----------------------
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrussia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alaba',
+      'Davies',
+      'Kimmich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lewandowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lewandowski', 'Gnarby', 'Lewandowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+//========= Solution of Callenge #2 ==================================
+// 1. Loop over the game.scored array and print each player name to the console,
+// along with the goal number
+//------ (Example: "Goal 1: Lewandowski")
+for (const scoredPlayer of game.scored.entries()) {
+  console.log(`Goal ${scoredPlayer[0] + 1}: ${scoredPlayer[1]}`);
+}
+
+// 2. Use a loop to calculate the average odd and log it to the console (We already
+// studied how to calculate averages, you can go check if you don't remember)
+// 3. Print the 3 odds to the console, but in a nice formatted way, exactly like this:
+// Odd of victory Bayern Munich: 1.33
+// Odd of draw: 3.25
+// Odd of victory Borrussia Dortmund: 6.5
+// Get the team names directly from the game object, don't hardcode them
+// (except for "draw"). Hint: Note how the odds and the game objects have the
+// same property names
+// 4. Bonus: Create an object called 'scorers' which contains the names of the
+// players who scored as properties, and the number of goals as the value. In this
+// game, it will look like this:
+// {
+// Gnarby: 1,
+// Hummels: 1,
+// Lewandowski: 2
+// }

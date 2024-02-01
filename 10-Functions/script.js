5a60277404bce491533b7a24b7e784a13e4957ff
Mironov <mironov5712s@gmail.com>
2024-02-01 02:34:31 +0200

    Update script.js

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -2,7 +2,11 @@
 
 const bookings = [];
 
-const createBooking = function (flightNum, numPassengers, price) {
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
   const booking = {
     flightNum,
     numPassengers,
@@ -11,7 +15,9 @@ const createBooking = function (flightNum, numPassengers, price) {
 
   console.log(booking);
   bookings.push(booking);
-  console.log(bookings);
 };
 
 createBooking('LH7203');
+createBooking('LH7205', 2);
+createBooking('LH7206', undefined, 1000);
+createBooking('LH7207', undefined);

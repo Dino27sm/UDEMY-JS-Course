b6f7b25a689719af6efdba638f4f07c41639ee30
Mironov <mironov5712s@gmail.com>
2024-02-21 12:06:35 +0200

    Update script.js

--- 11. Working with Arrays/script.js
+++ 11. Working with Arrays/script.js
@@ -64,16 +64,18 @@ const inputClosePin = document.querySelector('.form__input--pin');
 //============== DOM Manupulations =========================================
 //
 const displayMovements = function (movements) {
+  containerMovements.innerHTML = ''; // In this way old elements are deleted
+
   movements.forEach(function (mov, i) {
     const movType = mov < 0 ? 'withdrawal' : 'deposit';
-    const html = `<div class="movements__row">
+    const htmlStr = `<div class="movements__row">
     <div class="movements__type movements__type--${movType}">${
       i + 1
     } ${movType}</div>
-    <div class="movements__value">${mov}€</div>
+    <div class="movements__value">${Math.abs(mov)}€</div>
   </div>`;
 
-    containerMovements.insertAdjacentHTML('afterbegin', html);
+    containerMovements.insertAdjacentHTML('afterbegin', htmlStr);
   });
 };
 

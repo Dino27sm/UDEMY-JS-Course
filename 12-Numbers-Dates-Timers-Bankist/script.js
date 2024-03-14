410701ad1c3239702f31b8d4b124484423847d7c
Mironov <mironov5712s@gmail.com>
2024-03-14 10:56:32 +0200

    Update script.js

--- 12-Numbers-Dates-Timers-Bankist/script.js
+++ 12-Numbers-Dates-Timers-Bankist/script.js
@@ -251,3 +251,9 @@ btnSort.addEventListener('click', function (e) {
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
+console.log(3 === 3.0);
+console.log(0.1 + 0.2);
+console.log(0.1 + 0.2 === 0.3); // Result is "false"
+
+console.log(Number('0.7'));
+console.log(+'0.78');

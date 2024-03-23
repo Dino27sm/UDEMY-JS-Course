d26fd8bb0c31e95d55ec7de74c19b6d8da91ca5d
Mironov <mironov5712s@gmail.com>
2024-03-23 17:46:45 +0200

    Update script.js

--- 13-Advanced DOM and Events/script.js
+++ 13-Advanced DOM and Events/script.js
@@ -29,3 +29,20 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+////======================================================================
+// Selecting Elements
+//
+console.log(document.documentElement);
+console.log(document.head);
+console.log(document.body);
+
+document.querySelector('.header');
+const allSections = document.querySelectorAll('.section');
+console.log(allSections);
+
+console.log(document.querySelector('#section--1'));
+console.log(document.getElementById('section--1'));
+console.log(document.getElementsByClassName('btn'));
+
+const allButtons = document.getElementsByTagName('button');
+console.log(allButtons);

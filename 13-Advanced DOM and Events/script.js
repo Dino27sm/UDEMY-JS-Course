67c025ccb4cea67252a8204bcb2e3186dd9e17f9
Mironov <mironov5712s@gmail.com>
2024-04-01 05:15:45 +0300

    Update script.js

--- 13-Advanced DOM and Events/script.js
+++ 13-Advanced DOM and Events/script.js
@@ -196,25 +196,34 @@ document.addEventListener('keydown', function (e) {
 //     document.querySelector(idElm).scrollIntoView({ behavior: 'smooth' });
 //   }
 // });
-//========================= DOM Traversing ============================
-//------ Go down to CHILD Elements
+// //========================= DOM Traversing ============================
+// //------ Go down to CHILD Elements
 const h1 = document.querySelector('h1');
 console.log(h1);
 
-console.log(h1.querySelectorAll('.highlight'));
-console.log(h1.childNodes);
-console.log(h1.children);
+// console.log(h1.querySelectorAll('.highlight'));
+// console.log(h1.childNodes);
+// console.log(h1.children);
 
-h1.firstElementChild.setAttribute('style', 'color: white');
-h1.lastElementChild.setAttribute('style', 'color: orangered');
-//
-//------ Go up to PARENT Elements
-console.log(h1.parentNode);
-console.log(h1.parentElement);
+// h1.firstElementChild.setAttribute('style', 'color: white');
+// h1.lastElementChild.setAttribute('style', 'color: orangered');
+// //
+// //------ Go up to PARENT Elements
+// console.log(h1.parentNode);
+// console.log(h1.parentElement);
 
-h1.closest('.header').setAttribute(
-  'style',
-  'background: var(--gradient-secondary)'
-);
+// h1.closest('.header').setAttribute(
+//   'style',
+//   'background: var(--gradient-secondary)'
+// );
 
-h1.closest('h1').setAttribute('style', 'background: var(--gradient-primary)');
+// h1.closest('h1').setAttribute('style', 'background: var(--gradient-primary)');
+// //
+//-------- Go sideway to Siblings
+console.log(h1.previousElementSibling);
+console.log(h1.nextElementSibling);
+
+console.log(h1.previousSibling);
+console.log(h1.nextSibling);
+
+console.log(h1.parentElement.children); // To get all siblings of "h1"

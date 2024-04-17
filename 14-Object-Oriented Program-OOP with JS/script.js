3412cbad5656fe54d0d8790bb5383fce8429ef11
Mironov <mironov5712s@gmail.com>
2024-04-17 15:09:23 +0300

    Update script.js

--- 14-Object-Oriented Program-OOP with JS/script.js
+++ 14-Object-Oriented Program-OOP with JS/script.js
@@ -1,1 +1,20 @@
 'use strict';
+//
+const Person = function (firstName, birthYear) {
+  // Instance properties
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+};
+
+const dino = new Person('Dino', 1997);
+console.log(dino);
+
+// 1. New empty object is created "{}"
+// 2. function is called, "this" = {}
+// 3. {} linked to prototype
+// 4. function automaticaly return "{}"
+
+const matilda = new Person('Matilda', 1967);
+const jack = new Person('Jack', 1972);
+
+console.log(matilda, jack);

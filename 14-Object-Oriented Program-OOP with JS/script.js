a0b9e66f26078300d41c75f6324f45ae24712128
Mironov <mironov5712s@gmail.com>
2024-04-19 03:11:20 +0300

    Update script.js

--- 14-Object-Oriented Program-OOP with JS/script.js
+++ 14-Object-Oriented Program-OOP with JS/script.js
@@ -27,6 +27,21 @@ console.log(Person.prototype);
 
 Person.prototype.calcAge = function () {
   console.log(2037 - this.birthYear);
+  // "this" points to the object which calls the function "calcAge"
 };
 
 dino.calcAge();
+matilda.calcAge();
+
+console.log(dino.__proto__);
+console.log(dino.__proto__ === Person.prototype);
+// Which means that "dino.__proto__" is the same as "Person.prototype"
+//
+console.log(Person.prototype.isPrototypeOf(dino));
+console.log(Person.prototype.isPrototypeOf(matilda));
+console.log(Person.prototype.isPrototypeOf(Person));
+// !!! "Person.prototype" is not a prototype of "Person" object.
+// It is a prototype of each object created by constructor function "Person"
+
+console.log(dino.__proto__.isPrototypeOf(dino));
+console.log(dino.__proto__.isPrototypeOf(matilda));

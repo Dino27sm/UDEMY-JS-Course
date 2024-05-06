34172f0ba4f46c81e444415eeabe955284024274
Mironov <mironov5712s@gmail.com>
2024-05-06 08:57:02 +0300

    Update script.js

--- 14-Object-Oriented Program-OOP with JS/script.js
+++ 14-Object-Oriented Program-OOP with JS/script.js
@@ -489,3 +489,21 @@ const PersonProto = {
 };
 
 const steven = Object.create(PersonProto);
+
+const StudentProto = Object.create(PersonProto);
+
+StudentProto.init = function (firstName, birthYear, course) {
+  PersonProto.init.call(this, firstName, birthYear);
+  this.course = course;
+};
+
+StudentProto.introduce = function () {
+  console.log(`My name is ${this.firstName} and I study ${this.course}.`);
+};
+
+const jay = Object.create(StudentProto);
+jay.init('Jay', 2010, 'Computer Science');
+
+console.log(jay);
+jay.calcAge();
+jay.introduce();

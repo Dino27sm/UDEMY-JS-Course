00a1cb9717645e3a385711bc48608fcb7ccae319
Mironov <mironov5712s@gmail.com>
2024-05-09 16:23:39 +0300

    Update script.js

--- 15-Mapty/script.js
+++ 15-Mapty/script.js
@@ -10,3 +10,17 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+    },
+    function () {
+      alert('Cannot get your position!');
+    }
+  );
+}

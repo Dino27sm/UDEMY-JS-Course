d82978fa5c89163df89c3d14139011ffce57d058
Mironov <mironov5712s@gmail.com>
2024-06-10 12:47:25 +0300

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -4,3 +4,13 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const request = new XMLHttpRequest();
+request.open(
+  'GET',
+  'https://countries-api-836d.onrender.com/countries/name/bulgaria'
+);
+request.send();
+
+request.addEventListener('load', function () {
+  console.log(this.responseText);
+});

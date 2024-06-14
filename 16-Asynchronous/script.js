82095287fecaa8fba9d09b0197a82e9d615319ef
Mironov <mironov5712s@gmail.com>
2024-06-14 13:44:20 +0300

    Update script.js

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -98,36 +98,52 @@ const renderCountry = function (data, className = '') {
 // };
 //============================================================================
 //
-//=========================== Get Neighbour Country ==========================
+// //=========================== Get Neighbour Country ==========================
+// //
+// const getCountryData = function (countryName) {
+//   // Get Main (first) Country
+//   const promiceFetch = fetch(
+//     `https://countries-api-836d.onrender.com/countries/name/${countryName}`
+//   );
+//   const promiceJSON = promiceFetch.then(function (responseFetch) {
+//     return responseFetch.json(); // "json()" returns a "promise"
+//   });
+
+//   // Get Neighbour (second) Country
+//   const promisNeighbour = promiceJSON.then(function (jsonData) {
+//     renderCountry(jsonData[0]);
+//     const neighbour = jsonData[0].borders[0];
+//     return fetch(
+//       `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+//     );
+//   });
+//   const promisNeighbourJSON = promisNeighbour.then(function (
+//     responseNeighbour
+//   ) {
+//     return responseNeighbour.json();
+//   });
+
+//   // Render Neighbour Country
+//   promisNeighbourJSON.then(function (neighbourData) {
+//     renderCountry(neighbourData, 'neighbour');
+//   });
+// };
+// //===========================================================================
+//
+//================ Get Neighbour Country Using Array Functions =================
 //
 const getCountryData = function (countryName) {
-  // Get Main (first) Country
-  const promiceFetch = fetch(
-    `https://countries-api-836d.onrender.com/countries/name/${countryName}`
-  );
-  const promiceJSON = promiceFetch.then(function (responseFetch) {
-    return responseFetch.json(); // "json()" returns a "promise"
-  });
-
-  // Get Neighbour (second) Country
-  const promisNeighbour = promiceJSON.then(function (jsonData) {
-    renderCountry(jsonData[0]);
-    const neighbour = jsonData[0].borders[0];
-    return fetch(
-      `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
-    );
-  });
-  const promisNeighbourJSON = promisNeighbour.then(function (
-    responseNeighbour
-  ) {
-    return responseNeighbour.json();
-  });
-
-  // Render Neighbour Country
-  promisNeighbourJSON.then(function (neighbourData) {
-    renderCountry(neighbourData, 'neighbour');
-  });
+  fetch(`https://countries-api-836d.onrender.com/countries/name/${countryName}`)
+    .then(response => response.json())
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+      return fetch(
+        `https://countries-api-836d.onrender.com/countries/alpha/${neighbour}`
+      );
+    })
+    .then(resposeNeighbour => resposeNeighbour.json())
+    .then(dataNeighbour => renderCountry(dataNeighbour, 'neighbour'));
 };
 //===========================================================================
-
 getCountryData('portugal');

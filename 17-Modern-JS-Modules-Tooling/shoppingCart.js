659821bcf4dbac13c6e4ccab992905220c95959c
Mironov <mironov5712s@gmail.com>
2024-07-22 11:19:39 +0300

    Update shoppingCart.js

--- 17-Modern-JS-Modules-Tooling/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/shoppingCart.js
@@ -1,21 +1,3 @@
-const num1 = 13;
-const num2 = 27;
-const cartContent = [];
-
-const getSum = function (n1, n2) {
-  return n1 + n2;
-};
-
-const cartTotal = function (productName) {
-  return cartContent.push(productName);
-};
-
-// const dspFromCart = 'Displayed from shoppingCart';
-// export { getSum };
-
-export { cartContent };
-
-cartTotal('bread');
-cartTotal('tomato');
-cartTotal('potato');
-cartTotal('apple');
+//=========== EXPORTING Module ======================
+//
+console.log('From EXPORTING Module.');

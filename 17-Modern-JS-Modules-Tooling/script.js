9661e6dafee38f0a118c306e2d991fe0b9457dd3
Mironov <mironov5712s@gmail.com>
2024-07-22 11:19:41 +0300

    Update script.js

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -1,3 +1,4 @@
-import { cartContent } from './shoppingCart.js';
-console.log(`Received from "shoppingCart.js" module: ${cartContent[3]}`);
-console.log(cartContent);
+//=========== IMPORTING Module ======================
+//
+import './shoppingCart.js'; // First this module file is executed
+console.log('From IMPORTING Module.');

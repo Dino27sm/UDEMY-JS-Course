d3a917797ee12a4ca1e352c3bf51b706fa3b1902
Mironov <mironov5712s@gmail.com>
2024-07-22 12:00:52 +0300

    Update shoppingCart.js

--- 17-Modern-JS-Modules-Tooling/shoppingCart.js
+++ 17-Modern-JS-Modules-Tooling/shoppingCart.js
@@ -1,15 +1,23 @@
 //=========== EXPORTING Module ======================
-//
-console.log('From EXPORTING Module.');
+// // Named Exports ------------------------------------
+// //
+// console.log('From EXPORTING Module.');
 
-const shippingCost = 10;
+// const shippingCost = 10;
 const cart = [];
 
-export const addToCart = function (product, quantity) {
+// export const addToCart = function (product, quantity) {
+//   cart.push({ product, quantity });
+//   console.log(`${quantity} ${product} added th the cart.`);
+// };
+
+// const num1 = 13;
+// const num2 = 22;
+// export { num1, num2 };
+// //
+// Default Exports - Used to Export only one thing's value
+//
+export default function (product, quantity) {
   cart.push({ product, quantity });
   console.log(`${quantity} ${product} added th the cart.`);
-};
-
-const num1 = 13;
-const num2 = 22;
-export { num1, num2 };
+}

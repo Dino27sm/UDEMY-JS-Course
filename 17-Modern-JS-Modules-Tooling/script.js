537227b885e2ad88f2a355d74776ffbf973f64c0
Mironov <mironov5712s@gmail.com>
2024-07-22 11:47:26 +0300

    Update script.js

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -2,7 +2,13 @@
 //
 // import './shoppingCart.js'; // First this module file is executed
 // console.log('From IMPORTING Module.');
-import { addToCart, num1, num2 } from './shoppingCart.js';
+// import { addToCart, num1, num2 } from './shoppingCart.js';
 
-addToCart('bread', 3);
-console.log(num1, num2);
+// addToCart('bread', 3);
+// console.log(num1, num2);
+//
+// To Import everything from "shoppingCart" as ONE Object
+import * as ShoppingCart from './shoppingCart.js';
+
+console.log(ShoppingCart.num1, ShoppingCart.num2);
+ShoppingCart.addToCart('potato', 7);

41c35f6dde2cc8aa3f8ec25ec61a7932065c2a92
Mironov <mironov5712s@gmail.com>
2024-08-13 10:14:44 +0300

    Update controller.js

--- 18-Forkify/src/js/controller.js
+++ 18-Forkify/src/js/controller.js
@@ -13,14 +13,6 @@ import 'core-js/stable'; // For polyfilling everything else
 //
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // This API has been created by Jonas Schmedtmann !!!
 // https://forkify-api.herokuapp.com/v2
 

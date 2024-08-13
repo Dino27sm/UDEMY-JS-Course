d232beeb1fda97ddd9fa251ceb3a16313f80b9a8
Mironov <mironov5712s@gmail.com>
2024-08-13 10:37:14 +0300

    Files updated by Parcel

--- 18-Forkify/dist/index.e37f48ea.js
+++ 18-Forkify/dist/index.e37f48ea.js
@@ -2533,12 +2533,16 @@ const loadRecipe = async function(id) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "API_URL", ()=>API_URL);
+parcelHelpers.export(exports, "TIMEOUT_SEC", ()=>TIMEOUT_SEC);
 const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
+const TIMEOUT_SEC = 10;
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"hGI1E":[function(require,module,exports) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 parcelHelpers.export(exports, "getJSON", ()=>getJSON);
+var _regeneratorRuntime = require("regenerator-runtime");
+var _configJs = require("./config.js");
 const timeout = function(s) {
     return new Promise(function(_, reject) {
         setTimeout(function() {
@@ -2550,7 +2554,7 @@ const getJSON = async function(url) {
     try {
         const resp = await Promise.race([
             fetch(url),
-            timeout(10)
+            timeout((0, _configJs.TIMEOUT_SEC))
         ]);
         const data = await resp.json();
         if (!resp.ok) throw new Error(`${data.message}---(${resp.status})`);
@@ -2560,7 +2564,7 @@ const getJSON = async function(url) {
     }
 };
 
-},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"l60JC":[function(require,module,exports) {
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3","./config.js":"k5Hzs","regenerator-runtime":"dXNgZ"}],"l60JC":[function(require,module,exports) {
 // Inform "parcel" where icons come from ------------------
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);

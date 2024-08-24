d0540d244822e9971961542a821804190c800eeb
Mironov <mironov5712s@gmail.com>
2024-08-24 14:16:49 +0300

    Update resultsView.js

--- 18-Forkify/src/js/views/resultsView.js
+++ 18-Forkify/src/js/views/resultsView.js
@@ -2,7 +2,7 @@ import View from './View.js';
 //
 class ResultsView extends View {
   _parentElement = document.querySelector('.results');
-  _errorMessage = `No recipes found for your query! Please, try again :)`;
+  _errorMessage = `No recipes found for your query! Please, try again! 😒`;
   _message = '';
 
   _generateMarkup() {

374520b8de9a730a2dc0d31ee119b4b23301be4a
Mironov <mironov5712s@gmail.com>
2024-08-25 06:50:57 +0300

    Update paginationView.js

--- 18-Forkify/src/js/views/paginationView.js
+++ 18-Forkify/src/js/views/paginationView.js
@@ -5,24 +5,50 @@ class PaginationView extends View {
   _parentElement = document.querySelector('.pagination');
 
   _generateMarkup() {
+    const currentPage = this._data.page;
     const numPages = Math.ceil(
       this._data.results.length / this._data.resultsPerPage
     );
     console.log(numPages);
     // 1. Page 1, and there are other pages
-    if (this._data.page === 1 && numPages > 1) {
-      return `Page 1 and other pages.`;
+    if (currentPage === 1 && numPages > 1) {
+      return `
+        <button class="btn--inline pagination__btn--next">
+          <span>Page ${currentPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>`;
     }
     // 2. Last page
-    if (this._data.page === numPages && numPages > 1) {
-      return `Last page.`;
+    if (currentPage === numPages && numPages > 1) {
+      return `
+        <button class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${currentPage - 1}</span>
+        </button>`;
     }
     // 3. Other page
-    if (this._data.page < numPages) {
-      return `Other pages.`;
+    if (currentPage < numPages) {
+      return `
+        <button class="btn--inline pagination__btn--prev">
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-left"></use>
+          </svg>
+          <span>Page ${currentPage - 1}</span>
+        </button>
+        <button class="btn--inline pagination__btn--next">
+          <span>Page ${currentPage + 1}</span>
+          <svg class="search__icon">
+            <use href="${icons}#icon-arrow-right"></use>
+          </svg>
+        </button>
+      `;
     }
     // 4. Page 1, and NO other pages
-    return `Page 1 and NO other pages.`;
+    return ``; // No pages to go
   }
 }
 

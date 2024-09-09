c47024a5e2b239d399af3d643a70fb884aa71797
Mironov <mironov5712s@gmail.com>
2024-09-09 17:15:28 +0300

    Update controller.js

--- 18-Forkify/src/js/controller.js
+++ 18-Forkify/src/js/controller.js
@@ -88,9 +88,21 @@ const controlServings = function (newServings) {
   recipeView.update(model.state.recipe); // Updates only the changes in Servings
 };
 
+const controlAddBookmark = function () {
+  if (!model.state.recipe.bookmarked) {
+    model.addBookmark(model.state.recipe);
+  } else {
+    model.deleteBookmark(model.state.recipe.id);
+  }
+
+  console.log(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
   recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerAddBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick_PageBtn(controlPagination);
 };

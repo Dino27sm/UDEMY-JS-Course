c81f22d075e3078ce4acf39610b06a932cc7c101
Mironov <mironov5712s@gmail.com>
2024-10-08 15:57:29 +0300

    Update config.js

--- 18-Forkify/src/js/config.js
+++ 18-Forkify/src/js/config.js
@@ -3,3 +3,4 @@ export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes`;
 export const API_KEY = '8c8d3385-32de-452b-b7e5-1d0eebc529db';
 export const TIMEOUT_SEC = 12;
 export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
